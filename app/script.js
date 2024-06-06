a4401aa1b13af7d63be4ce5fb2c9bee7ef24779a
Maycol Teles <maycolteles@hotmail.com>
2024-06-05 22:24:10 -0300

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -8,15 +8,27 @@ function closeDrawer() {
 }
 
 function setLightTheme() {
+    localStorage.removeItem('theme');
+    localStorage.currentTheme = "light";
     document.documentElement.style.setProperty('--cor-sombra', 'white');
     document.documentElement.style.setProperty('--cor-text', 'black');
     document.documentElement.style.setProperty('--cor-background1', 'white');
-    document.documentElement.style.setProperty('--cor-background2', '#3a9bdc59');
+    document.documentElement.style.setProperty('--cor-background2', '#0a5a9b');
 }
 
 function setDarkTheme() {
+    localStorage.removeItem('theme');
+    localStorage.currentTheme = "dark";
     document.documentElement.style.setProperty('--cor-sombra', 'black');
     document.documentElement.style.setProperty('--cor-text', 'white');
-    document.documentElement.style.setProperty('--cor-background1', 'black');
-    document.documentElement.style.setProperty('--cor-background2', 'black');
+    document.documentElement.style.setProperty('--cor-background1', '#191e23');
+    document.documentElement.style.setProperty('--cor-background2', '#064b79');
+}
+
+var theme = window.localStorage.currentTheme;
+
+if (theme === "dark") {
+    setDarkTheme();
+} else {
+    setLightTheme();
 }
